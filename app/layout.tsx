357e4e70a9608cf7a67b275624b5bdf1a6eba9f1
Yussin R. Hidayat <yussin.hidayat@gmail.com>
2024-04-26 14:56:05 +0300

    update entity

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <script src="https://cdn.jsdelivr.net/npm/react/umd/react.production.min.js" crossOrigin="anonymous"></script>
+
+        <script
+          src="https://cdn.jsdelivr.net/npm/react-dom/umd/react-dom.production.min.js"
+          crossOrigin="anonymous"></script>
+
+        <script
+          src="https://cdn.jsdelivr.net/npm/react-bootstrap@next/dist/react-bootstrap.min.js"
+          crossOrigin="anonymous"></script>
+
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,179 @@
-import Image from 'next/image'
+"use client";
+
+import { useEffect, useState } from 'react';
+import { Button, Form, Modal, Table } from 'react-bootstrap';
+import Moment from 'moment';
+
+class Principal {
+  id: number;
+  username: string;
+  email: string;
+  createdDate: string;
+  updatedDate: string;
+}
+
+function findById(id: number, entities: Principal[] | undefined) {
+  if (entities) {
+    for (let i = 0; i < entities.length; i++) {
+      let entity = entities[i];
+      if (entity.id == id) {
+        return entity;
+      }
+    }
+  }
+}
+
+function parseResponse(data: Response) {
+  if (data.ok) {
+    return data.json();
+  }
+
+  throw new Error('Something went wrong');
+}
 
 export default function Home() {
+  const [show, setShow] = useState(false);
+  const [entity, setEntity] = useState<Principal>(new Principal());
+  const [entities, setEntities] = useState<Principal[]>();
+
+  const loadEntities = () => {
+    fetch('http://localhost:5290/v1/principals')
+      .then(parseResponse)
+      .then(data => {
+        setEntities(data.data.content);
+      })
+      .catch(e => console.log(e));
+  }
+
+  useEffect(() => {
+    loadEntities();
+  }, []);
+
+  const closeModal = () => setShow(false);
+  const showModal = (id?: number) => {
+    let p;
+    if (id) {
+      p = findById(id, entities);
+    }
+
+    setEntity(p ? p : new Principal());
+    setShow(true);
+  };
+  const addEntity = () => {
+    fetch('http://localhost:5290/v1/principals', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(entity)
+    })
+      .then(parseResponse)
+      .then(() => {
+        loadEntities();
+        closeModal();
+      });
+  };
+  const editEntity = () => {
+    fetch('http://localhost:5290/v1/principals/' + entity.id, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(entity)
+    })
+      .then(parseResponse)
+      .then(() => {
+        loadEntities();
+        closeModal();
+      });
+  };
+  const removeEntity = (id: number) => {
+    fetch('http://localhost:5290/v1/principals/' + id, {
+      method: "DELETE"
+    })
+      .then(parseResponse)
+      .then(loadEntities);
+  };
+  const saveEntity = () => {
+    entity.id ? editEntity() : addEntity();
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className="mx-auto pt-5" style={{ "width": "800px" }}>
+      <h2>Principal List</h2>
+      <Table hover>
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Username</th>
+            <th scope="col">Email</th>
+            <th scope="col"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {entities && entities.map(p => <tr key={p.id}>
+            <td>{p.id}</td>
+            <td>{p.username}</td>
+            <td>{p.email}</td>
+            <td>
+              <Button className="mx-2" variant="outline-primary" onClick={() => showModal(p.id)}>Edit</Button>
+              <Button className="mx-2" variant="outline-danger" onClick={() => removeEntity(p.id)}>Delete</Button>
+            </td>
+          </tr>)}
+        </tbody>
+      </Table>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <Button variant="primary" onClick={() => showModal()}>
+        Add
+      </Button>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      <Modal show={show} onHide={closeModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>{entity.id ? "Edit" : "Add"} Principal</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            {
+              entity.id && <Form.Group className="mb-3">
+                <Form.Label>ID</Form.Label>
+                <Form.Control value={entity.id} readOnly />
+              </Form.Group>
+            }
+            <Form.Group className="mb-3" controlId="username">
+              <Form.Label>Username</Form.Label>
+              <Form.Control value={entity?.username} onChange={(e) => {
+                setEntity({ ...entity, username: e.target.value });
+              }} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="name">
+              <Form.Label>Email</Form.Label>
+              <Form.Control value={entity?.email} onChange={(e) => {
+                setEntity({ ...entity, email: e.target.value });
+              }} />
+            </Form.Group>
+            {
+              entity.id && <Form.Group className="mb-3">
+                <Form.Label>Created Date</Form.Label>
+                <Form.Control value={Moment(entity.createdDate).format('D MMM yyyy H:mm:ss')} readOnly />
+              </Form.Group>
+            }
+            {
+              entity.id && <Form.Group className="mb-3">
+                <Form.Label>Updated Date</Form.Label>
+                <Form.Control value={Moment(entity.createdDate).format('D MMM yyyy H:mm:ss')} readOnly />
+              </Form.Group>
+            }
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={closeModal}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={saveEntity}>
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 }
